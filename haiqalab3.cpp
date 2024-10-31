54c3897907f1e08b6bcd3c58d866caccc13c6b63
Haiqapervaiz <haiqapervaiz90@gmail.com>
2024-10-31 10:48:42 +0500

    Add files via upload

---
+++ haiqalab3.cpp
@@ -0,0 +1,55 @@
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "  *  " << endl;
+//	cout << "*****" << endl;
+//	cout << "  *  " << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << " *** " << endl;
+//	cout << " * * " << endl;
+//	cout << " *** " << endl;
+//	system("pause");
+//	return 0;
+//}
+
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "**********************************" << endl;
+//	cout << "*    Programming Assigment 1     *" << endl;
+//	cout << "*    Computer Programming        *" << endl;
+//	cout << "*        Author: ???             *" << endl;
+//	cout << "*  Due Date: Thursday, Jan . 24  *" << endl;
+////	system("pause");
+//	return 0;
+//}
+//
+//
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "ccccccccc                      ++                ++       " << endl;
+//	cout << "cc                             ++                ++       " << endl;
+//	cout << "cc                     ++++++++++++++       ++++++++++++++" << endl;
+//	cout << "cc                     ++++++++++++++       ++++++++++++++" << endl;
+//	cout << "cc                             ++                 ++      " << endl;
+//	cout << "ccccccccc                      ++                 ++      " << endl;
+//	system("pause");
+	/*return 0;*//*
+}*/
+
+
+
