4b4773ec97aeb68d9a460f6955ebbbee9cfe1a95
Haiqapervaiz <haiqapervaiz90@gmail.com>
2024-11-07 16:27:38 +0500

    Add files via upload

---
+++ itc lab 4.cpp
@@ -0,0 +1,92 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int var1 = 13; 
+//	cout << "value of variable is:" << var1 << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int var;
+//	cout << "enter variable value:" << endl;
+//	cin >> var;
+//	cout << "variable value entered is:" << var << endl;
+//	system("pause");
+//	return 0;
+//}  
+//
+//__
+//
+
+
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int a, b;
+//	cout << "enter value of first variable:"  << endl;
+//	cin >> a;
+//    cout << "enter value of second variable:"  << endl;
+//	cin >> b;
+//	cout << "sum of values:" << a + b << endl;
+//	cout << "difference of values:" << a - b << endl;
+//	cout << "product of values:" << a * b << endl;
+//	cout << "division of values:" << a / b << endl;
+//	system("pause");
+//	return 0;
+//} 
+
+//
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	cout << "enter a number:" << endl;
+//	cin >> n;
+//	cout << "%d x 1 =%d" << n << n * 1 << endl;
+//	cout << "%d x 2 =%d" << n << n * 2 << endl;
+//	cout << "%d x 3 =%d" << n << n * 3 << endl;
+//	cout << "%d x 4 =%d" << n << n * 4 << endl;
+//	cout << "%d x 5 =%d" << n << n * 5 << endl;
+//	cout << "%d x 6 =%d" << n << n * 6 << endl;
+//	cout << "%d x 7 =%d" << n << n * 7 << endl;
+//	cout << "%d x 8 =%d" << n << n * 8 << endl;
+//	cout << "%d x 9 =%d" << n << n * 9 << endl;
+//	cout << "%d x 10 =%d" << n << n * 10 << endl;
+//	system("pause");
+//	return 0;
+//}
+
+
+
+//
+//}
+//
+//#include<iostream>
+//using namespace std;
+//int main()
+//{
+//	int fuel, rent, bill;
+//	cout << "enter value of fuel:" << endl;
+//	cin >> fuel;
+//	cout << "enter value of rent:" << endl;
+//	cin >> rent;
+//	cout << "enter the value of bill:" << endl;
+//	cin >> bill;
+//	cout << "_____________________________________" << endl;
+//	cout << "fuel:\t" << fuel << endl;
+//	cout << "rent:\t" << rent << endl;
+//	cout << "bill:\t" << bill << endl;
+//	cout << "total:\t" << fuel + rent + bill << endl;
+//	cout << "_____________________________________" << endl;
+//	system("pause");
+//	return 0;
+//}
